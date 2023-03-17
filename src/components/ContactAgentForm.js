ce65949a06f7a0eee9ae44757bea89ee52767401
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2023-03-17 16:51:50 +0300

    I've made the form to send a message

--- src/components/ContactAgentForm.js
+++ src/components/ContactAgentForm.js
@@ -1,34 +1,126 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope  } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 const ContactAgentForm = () => {
+
+  const [msgError, setMsgError] = useState(null);
+  const [bootstrap, setBootstrap] = useState(null);
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [msgByUser, setMsgByUser] = useState(null);
+  const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  
+    return response.json();
+  };
+  
+  const postMessage = async (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    let data = {
+      name,
+      email,
+      phone,
+      message
+    };
+    try {
+      const response = await postData( `${process.env.REACT_APP_PROXY}/api/message`, data);
+      console.log(response);
+      if (response.success === true){
+        let msg = "Votre message a été envoyé avec succès";
+        let bootstrapClass = "alert alert-success";
+      setBootstrap(bootstrapClass)
+      setEmail('')
+      setMsgByUser('')
+      setMsgError(msg)
+      setName('')
+      setPhone('')
+      } else if(response.errors){
+       let msg = response.errors.phone || response.errors.message || response.errors.email || response.errors.name;
+       let bootstrapClass = "alert alert-danger";
+       setBootstrap(bootstrapClass)
+       setMsgError(msg)
+      }
+    } catch (error) {
+      console.log(error);
+      let msg = "Une erreur s'est produite lors de l'envoi du message.";
+      let bootstrapClass = "alert alert-danger";
+     setBootstrap(bootstrapClass)
+     setMsgError(msg)
+    }
+  };
+  
+  
   return (
-    <div className="bg-white widget border rounded">
-      <h3 className="h4 text-black widget-title mb-3">
-      contacter l'agent immobilier
-      </h3>
-      <form action="" className="form-contact-agent">
-        <div className="form-group">
-          <label htmlFor="name">Votre nom complet</label>
-          <input type="text" id="name" className="form-control" />
+<div className="bg-white widget border rounded">
+  <h3 className="h4 text-black widget-title mb-3">
+    contacter l'agent immobilier
+  </h3>
+  <form action="" className="form-contact-agent">
+    <div className="form-group">
+      <label htmlFor="name">Votre nom complet</label>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text">
+            <FontAwesomeIcon icon={faUser} />
+          </span>
         </div>
-        <div className="form-group">
-          <label htmlFor="phone">Téléphone</label>
-          <input type="text" id="phone" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Email <div style={{ color: "red" }}>(n'est pas obligatoire)</div>
-          </label>
-          <input type="email" id="email" className="form-control" />
-        </div>
-        <div className="form-group">
-          <input
-            type="submit"
-            id="phone"
-            className="btn btn-primary"
-            defaultValue="Envoyer le message"
-          />
-        </div>
-      </form>
+        <input type="text" id="name" className="form-control" value = {name} onChange = {(e) =>setName(e.target.value)}  />
+      </div>
     </div>
+    <div className="form-group">
+      <label htmlFor="phone">Téléphone</label>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text">+261</span>
+        </div>
+        <input type="number" id="phone" className="form-control"  value = {phone} onChange = {(e) =>setPhone(e.target.value)}/>
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="email">
+        Email <div style={{ color: "red" }}>(n'est pas obligatoire)</div>
+      </label>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </div>
+        <input type="email" id="email" className="form-control"  value = {email} onChange = {(e) =>setEmail(e.target.value)} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="message">Message <div style={{ color: "red" }}>(n'est pas obligatoire)</div></label>
+      <textarea id="message" className="form-control"></textarea>
+    </div>
+    <div className="form-group">
+      <input onClick={postMessage}
+        type="submit"
+        id="phone"
+        className="btn btn-primary"
+        defaultValue="Envoyer le message"
+        value = {msgByUser} onChange = {(e) =>setMsgByUser(e.target.value)}
+      />
+    </div>
+  </form>
+  {msgError && (
+        <div className={bootstrap}>
+          {msgError}
+        </div>
+      )}
+</div>
   );
 };
 
