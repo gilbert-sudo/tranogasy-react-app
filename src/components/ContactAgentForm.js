09edaddd9a18b6feed6b4d1bd2f64c8199e13222
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2023-03-18 15:58:13 +0300

    I've reviewed the form to post a message

--- src/components/ContactAgentForm.js
+++ src/components/ContactAgentForm.js
@@ -1,14 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope  } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-const ContactAgentForm = () => {
-
+const ContactAgentForm = ({propertyId}) => {
   const [msgError, setMsgError] = useState(null);
   const [bootstrap, setBootstrap] = useState(null);
-  const [name, setName] = useState(null);
+  const [name, setName] = useState('');
   const [email, setEmail] = useState(null);
-  const [phone, setPhone] = useState(null);
-  const [msgByUser, setMsgByUser] = useState(null);
+  const [phone, setPhone] = useState('');
+  const [msgByUser, setMsgByUser] = useState('');
   const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
       method: 'POST',
@@ -28,37 +27,69 @@ const ContactAgentForm = () => {
     const phone = document.getElementById('phone').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    let data = {
-      name,
-      email,
-      phone,
-      message
-    };
-    try {
-      const response = await postData( `${process.env.REACT_APP_PROXY}/api/message`, data);
-      console.log(response);
-      if (response.success === true){
-        let msg = "Votre message a été envoyé avec succès";
-        let bootstrapClass = "alert alert-success";
+    if (!name.trim() || !phone.trim()) {
+      let msg = "Le nom et numéro de téléphone sont obligatoires.";
+      let bootstrapClass = "alert alert-danger";
       setBootstrap(bootstrapClass)
-      setEmail('')
-      setMsgByUser('')
       setMsgError(msg)
-      setName('')
-      setPhone('')
-      } else if(response.errors){
-       let msg = response.errors.phone || response.errors.message || response.errors.email || response.errors.name;
-       let bootstrapClass = "alert alert-danger";
+      return;
+    }
+
+const phoneNumberRegex = /^(03[2,3,4,8])(\d{7})$|^(3[2,3,4,8])(\d{7})$/;
+const phoneNumber = phone;
+  
+  if (phoneNumberRegex.test(phoneNumber)) {
+    if (phoneNumber.length === 10 || phoneNumber.length === 9) {
+      let data = {
+        name,
+        email,
+        phone,
+        message
+      };
+      try {
+        const response = await postData( `${process.env.REACT_APP_PROXY}/api/message/`+propertyId, data);
+        console.log(response);
+        if (response.success === true){
+          let msg = "Votre message a été envoyé avec succès";
+          let bootstrapClass = "alert alert-success";
+        setBootstrap(bootstrapClass)
+        setEmail('')
+        setMsgByUser('')
+        setMsgError(msg)
+        setName('')
+        setPhone('')
+        } else if(response.errors){
+         let msg = response.errors.phone || response.errors.message || response.errors.email || response.errors.name;
+         let bootstrapClass = "alert alert-danger";
+         setBootstrap(bootstrapClass)
+         setMsgError(msg)
+        }else{
+          let msg = response.error;
+          let bootstrapClass = "alert alert-danger";
+          setBootstrap(bootstrapClass)
+          setMsgError(msg)
+        }
+      } catch (error) {
+        console.log(error);
+        let msg = "Une erreur s'est produite lors de l'envoi du message.";
+        let bootstrapClass = "alert alert-danger";
        setBootstrap(bootstrapClass)
        setMsgError(msg)
       }
-    } catch (error) {
-      console.log(error);
-      let msg = "Une erreur s'est produite lors de l'envoi du message.";
+    } else {
+      // Phone number has invalid length
+      let msg = "votre numéro de téléphone n'est pas validé.";
       let bootstrapClass = "alert alert-danger";
-     setBootstrap(bootstrapClass)
-     setMsgError(msg)
+      setBootstrap(bootstrapClass)
+      setMsgError(msg)
     }
+  } else {
+    // Phone number has invalid format
+    let msg = "votre numéro de téléphone n'est pas validé.";
+    let bootstrapClass = "alert alert-danger";
+    setBootstrap(bootstrapClass)
+    setMsgError(msg)
+  }
   };
   
   
@@ -76,7 +107,7 @@ const ContactAgentForm = () => {
             <FontAwesomeIcon icon={faUser} />
           </span>
         </div>
-        <input type="text" id="name" className="form-control" value = {name} onChange = {(e) =>setName(e.target.value)}  />
+        <input type="text" id="name" className="form-control" value = {name} onChange = {(e) =>setName(e.target.value)} required = "ON" />
       </div>
     </div>
     <div className="form-group">
@@ -85,7 +116,7 @@ const ContactAgentForm = () => {
         <div className="input-group-prepend">
           <span className="input-group-text">+261</span>
         </div>
-        <input type="number" id="phone" className="form-control"  value = {phone} onChange = {(e) =>setPhone(e.target.value)}/>
+        <input  type="number" id="phone" className="form-control"  value = {phone} onChange = {(e) =>setPhone(e.target.value)} required ="ON"/>
       </div>
     </div>
     <div className="form-group">
@@ -103,7 +134,7 @@ const ContactAgentForm = () => {
     </div>
     <div className="form-group">
       <label htmlFor="message">Message <div style={{ color: "red" }}>(n'est pas obligatoire)</div></label>
-      <textarea id="message" className="form-control"></textarea>
+      <textarea   value = {msgByUser} onChange = {(e) =>setMsgByUser(e.target.value)} id="message" className="form-control"></textarea>
     </div>
     <div className="form-group">
       <input onClick={postMessage}
@@ -111,7 +142,6 @@ const ContactAgentForm = () => {
         id="phone"
         className="btn btn-primary"
         defaultValue="Envoyer le message"
-        value = {msgByUser} onChange = {(e) =>setMsgByUser(e.target.value)}
       />
     </div>
   </form>
